fdce688ada6be0f24e34ee350d56ba314b85732a
Alberto Paico <alberto.paico@qualifacts.com>
2025-10-23 19:25:05 -0500

    update values

--- pay_caculator_web/app.py
+++ pay_caculator_web/app.py
@@ -21,4 +21,4 @@ def index():
     return render_template("index.html", total=total)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    app.run(host="0.0.0.0",debug=True, port=5050)
